71dca9ab744b0cffc9db62302680c9e482e70b3c
aravindhalahalli <aravind@byteally.com>
2024-04-03 11:35:01 +0530

    Handling API response and showing in ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,67 @@
 "use client";
-import Card, { CardProps } from "@/components/Card";
-import PageTitle from "@/components/PageTitle";
-import React from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
+import { buildMenuHierarchy } from "@/lib/utils";
+import PageTitle from "@/components/PageTitle";
 
-const fetcher = (url: string | URL | Request) =>
-  fetch(url).then((res) => res.json());
+const fetcher = (url: string | URL | Request) => fetch(url).then((res) => res.json());
 
 export default function Home() {
+  const [expandedItems, setExpandedItems] = useState<string[]>([]);
+  const toggleExpand = (name: string) => {
+    if (expandedItems.includes(name)) {
+      setExpandedItems(expandedItems.filter((item) => item !== name));
+    } else {
+      setExpandedItems([...expandedItems, name]);
+    }
+  };
+
   const { data, error, isLoading } = useSWR(
     "https://demo6396395.mockable.io/bcf-boards",
     fetcher
   );
-  if (error) return <div>failed to load the bcf boards</div>;
-  if (isLoading) return <div>Loading Bcf Boards...</div>;
+  if (error) return <div>Failed to load the BCF boards</div>;
+  if (isLoading) return <div>Loading BCF Boards...</div>;
+
+  const Tree: React.FC<{ data: any }> = ({ data }) => {
+    const isExpanded = expandedItems.includes(data.name);
+    const isParent = data.children && data.children.length > 0;
+
+    return (
+      <div className="my-1" style={{ paddingLeft: 10 }}>
+        <span
+          onClick={() => toggleExpand(data.name)}
+          className={`text-lg font-medium ${
+            isParent ? "text-blue-600 cursor-pointer" : "text-green-600 cursor-text"
+          }`}
+        >
+          {data.name}
+        </span>
+        {isExpanded ? (
+          <div style={{ paddingLeft: 10 }}>
+            {data?.children?.map((child: any, id: number) => (
+              <Tree key={id} data={child} />
+            ))}
+          </div>
+        ) : null}
+      </div>
+    );
+  };
+
+  const menu = buildMenuHierarchy(data.boards);
   return (
     <div className="flex flex-col gap-5 w-full">
       <PageTitle title="BCF Boards" />
       <section className="grid w-full grid-cols-1 gap-4 gap-x-8 transition-all sm:grid-cols-2 xl:grid-cols-6">
-        {data.map((d: CardProps, i: React.Key | null | undefined) => (
-          <Card key={i} id={d.id} name={d.name} createdAt={d.createdAt} />
-        ))}
+        <div className="flex justify-evenly flex-col">
+          {menu?.map((d: any, i: number) => (
+            <Tree key={i} data={d} />
+          ))}
+        </div>
       </section>
     </div>
   );
 }
+
+
+
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,49 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+
+interface BCFBoard {
+  id: number;
+  name: string;
+  createdAt: string;
+}
+
+interface BCF {
+  id: number;
+  name: string;
+  createdAt: string;
+  bcfBoards: BCFBoard[];
+}
+
+interface UseCase {
+  id: number;
+  name: string;
+  createdAt: string;
+  bcfs: BCF[];
+}
+
+export function buildMenuHierarchy(boards: UseCase[]) {
+  return boards.map((board) => ({
+    name: board.name,
+    children: buildBcfHierarchy(board.bcfs),
+  }));
+}
+
+export function buildBcfHierarchy(bcfs: BCF[]) {
+  return bcfs.map((bcf) => ({
+    name: bcf.name,
+    children: buildBcfBoardHierarchy(bcf.bcfBoards),
+  }));
+}
+
+export function buildBcfBoardHierarchy(bcfBoards: BCFBoard[]) {
+  return bcfBoards.map((bcfBoard) => ({
+    name: bcfBoard.name,
+  }));
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
